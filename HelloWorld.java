14ca127560dc2c0572e6a8dc14aeb5edaedac53c
bvarshi <103553647+bvarshi@users.noreply.github.com>
2024-01-16 16:07:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("varshithabysani-blore");
+    System.out.println("varshitha ananya");
   }
 }
